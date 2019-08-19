5d8d2a029b15991ff3fe2841a0cb431e8270ecc2
Angelika Fogasova <angelika.fogasova@gmail.com>
2019-08-19 09:04:50 +0200

    Final commit before week8 modifications

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://localhost:8080', //https://todoapp-c0e97.firebaseio.com
+    baseURL: 'http://localhost:8080', //https://todoapp-c0e97.firebaseio.com; https://mighty-wildwood-75803.herokuapp.com/
 });
 
 export default instance
